98695d2a38827db32b04e62fe118755b3522a69f
kiyoung <popping2606@gmail.com>
2020-08-22 20:49:05 +0900

    making..

---
+++ src/pages/PostListPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '../components/common/Button'
+const PostListPage = () => {
+	return (
+		<>
+			<div>포스트 리스트</div>
+			<Button>버튼</Button>
+		</>
+	)
+}
+
+export default PostListPage
